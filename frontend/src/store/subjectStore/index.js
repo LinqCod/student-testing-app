c692b8d7db974b9a65b36de7a3851f6409a8ecc8
linqcod <123321The_Sam321>
2022-12-14 01:06:18 +0300

    init student login page

--- frontend/src/store/subjectStore/index.js
+++ frontend/src/store/subjectStore/index.js
@@ -17,7 +17,7 @@ export default {
             state.subjects.push(subject);
         },
         DELETE_SUBJECT(state, id) {
-            state.subjects = state.subjects.filter(subject => subject.id != id);
+            state.subjects = state.subjects.filter(subject => subject.id !== id);
         }
     },
     actions: {
