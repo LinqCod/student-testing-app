376584c24a7eab09be3576261679bdd37989a564
linqcod <123321The_Sam321>
2022-12-13 15:29:30 +0300

    init frontend

---
+++ frontend/src/views/Subjects.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="subjects">
+    <h1>Subjects</h1>
+    <subjects-list :subjects="subjects"></subjects-list>
+  </div>
+</template>
+<script>
+import SubjectsList from "@/components/SubjectsList.vue";
+export default
+{  name: "Subjects",
+  components: { SubjectsList  },
+  data: function() {
+    return {
+      subjects: [
+        {
+          id: 1,
+          title: "Математика"
+        },
+        {
+          id: 2,
+          title: "Программирование"
+        }
+      ]
+    };
+  }};
+</script>
